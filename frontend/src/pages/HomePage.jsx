39bcdfc66e0f73795f2828bae51d7d8d6dddbdd1
Srijan Sapkota <srijan.797402@memc.tu.edu.np>
2025-09-18 10:16:57 +0545

    Sidebar error fixed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useChatStore } from "../store/useChatStore";
 import NoChatSelected from "../components/NoChatSelected";
-import Sidebar from "../components/SidebardeBar";
+import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
